2c4b514d5bda1cc4ed8897a73c272000e29dba1d
Salman Farsi <salmanfarsi9005@gmail.com>
2026-09-12 11:17:09 +0600

    feat: taking input in python

---
+++ introduction-to-python-for-ml/python-basics/taking_input.py
@@ -0,0 +1,19 @@
+name = input("Enter your name: ")
+print(name, type(name))
+
+age = input("Enter your age: ")
+print(age, type(age))
+
+# problem: age is coming as string
+
+# solution:
+age = int(age)
+print(age, type(age))
+
+
+height = float(input("Enter your height: "))
+height = height + 0.2
+print(height, type(height));
+
+
+
