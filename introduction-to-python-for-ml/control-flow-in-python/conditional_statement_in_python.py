563d83a21c9121a69fab3e4544222d39ec9dbd75
Salman Farsi <salmanfarsi9005@gmail.com>
2026-09-12 12:03:40 +0600

    feat: conditional statement in python

---
+++ introduction-to-python-for-ml/control-flow-in-python/conditional_statement_in_python.py
@@ -0,0 +1,13 @@
+raining = False
+muddy = True
+
+if raining == True:
+    print("Rain coat is necessary")
+    if muddy == True:
+        print("Sandal is necessary")
+    else:
+        print("Bring Sneakers")
+else: 
+    print("Go without a rain coat")
+    print("This is a sunny day")
+
