ea919d5daf58ec321f2ed593231ef1794afcb25d
Salman Farsi <salmanfarsi9005@gmail.com>
2026-09-13 02:58:29 +0600

    feat: string, indexing and slicing

---
+++ introduction-to-python-for-ml/string-and-list/string_indexing_and_slicing.py
@@ -0,0 +1,16 @@
+prompt = "Explain string indexing and slicing in Python"
+print(prompt)
+print(type(prompt))
+
+message = """tell me about yourself 
+what are the issues you've been facing while learning to code 
+how are you tackling them"""
+
+first_message = message[0:24:2]
+print(first_message)
+
+string = "hello world"
+print(string[7])
+
+
+
