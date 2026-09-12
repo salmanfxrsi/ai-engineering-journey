79eebeb031509929ca9be87f6f351f2903073caf
Salman Farsi <salmanfarsi9005@gmail.com>
2026-09-12 12:15:59 +0600

    feat: for loop in python

---
+++ introduction-to-python-for-ml/control-flow-in-python/for_loop_in_python.py
@@ -0,0 +1,10 @@
+for i in range(1, 10):
+    for j in range(1, 5):
+        print(i, j)
+
+print(list(range(15)))
+
+# range(start, end, step)
+
+for i in range(1, 20, 2):
+    print(i, "Hello World")
