58ea36f3b4203deb1e4f5ff5d17ab493713e8ae2
Salman Farsi <salmanfarsi9005@gmail.com>
2026-09-13 22:07:36 +0600

    feat: string splitting joining and formatting

---
+++ introduction-to-python-for-ml/string-and-list/string_formatting.py
@@ -0,0 +1,10 @@
+name = "Salman Farsi"
+age = 19
+height = 5.32342
+profession = "AI ML Engineer"
+
+info = f"My name is {name}, I am {age} years old, my height is {height:.3} meters and I work as an {profession}."
+print(info)
+
+model_accuracy = 0.9567362
+print(f"The model accuracy is {model_accuracy:.2%}")
---
+++ introduction-to-python-for-ml/string-and-list/string_splitting_joining.py
@@ -0,0 +1,7 @@
+prompt = "welcome to python programming, python is a great language for beginners. python is also used in data science."
+
+tokens = prompt.split()
+print(tokens)
+
+sentence = " ".join(tokens) 
+print(sentence)
